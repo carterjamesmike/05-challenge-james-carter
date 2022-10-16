db6c22e8a5d65c1a3fc7763ce5334e372c7326a3
James Carter <carterjamesmike@gmail.com>
2022-10-16 14:53:28 -0500

    Added comments to script.js and updated readMe

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # 05-challenge-james-carter
+This is the fifth challange assignment for the UTA Full Stack Coding Bootcamp. The unique acceptance criteria for this challenge was to create a simple calender application for standard work-day hours that saves user input into local storage. The acceptance criteria is as follows:
+1. Current date is displayed at top of application
+2. Time blocks for standard buisness hours are present
+3. Time blocks are color-coded based off current time to indicate whether it is in the past, present, or future
+4. User's can input their schedule into timeblocks
+5. User input is saved into local storage
+6. Saved input persists when paged is refreshed or reloaded. 
+
+## Access
+-The deployed page can be accessed via: 
+-The code can be accessed via: https://github.com/carterjamesmike/05-challenge-james-carter
+
+## Screenshot of applicaiton
+![Screenshot of application](./assets/images/03-challenge-james-carter.jpg)
+
+## Credits
+N/A
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Variables for save buttons, display date, and array that houses user input
 var saveBtnEl0 = $('#btn-0');
 var saveBtnEl1 = $('#btn-1');
 var saveBtnEl2 = $('#btn-2');
@@ -8,9 +9,9 @@ var saveBtnEl6 = $('#btn-6');
 var saveBtnEl7 = $('#btn-7');
 var saveBtnEl8 = $('#btn-8');
 var displayDate = moment().format('dddd MMM Do YY')
-
 var arr = ["", "", "", "", "", "", "", "", "",];
 
+//Init fx that displays current date and sets local storage data into array and calls renderEvents if data is present
 function init () {
     document.getElementById('currentDay').innerHTML = displayDate;
     var storedEvents = JSON.parse(localStorage.getItem("arr"));
@@ -20,6 +21,7 @@ function init () {
     }
 }
 
+//Save fx that sets user input data into local storage
 function saveInput (id) {
     var eventInput = document.querySelector(`#e${id+9}`);  
     eventText = eventInput.value;
@@ -28,6 +30,7 @@ function saveInput (id) {
     renderEvents();
 }
 
+//Render fx that displays array data into input fields
 function renderEvents () {
     for (i = 0; i < arr.length; i++) {
         var inputText = document.querySelector(`#e${i+9}`);
@@ -35,52 +38,45 @@ function renderEvents () {
     }
 }
 
+//Button functionality to call saveInput fx
 saveBtnEl0.on('click', function() { 
     saveInput(0);
     id = 0;
-
-}) 
-  
+})   
 saveBtnEl1.on('click', function () {
     saveInput(1);
     id = 1;  
 })
-
 saveBtnEl2.on('click', function () {
     saveInput(2);
     id = 2;  
 })
-
 saveBtnEl3.on('click', function () {
     saveInput(3);
     id = 3;  
 })
-
 saveBtnEl4.on('click', function () {
     saveInput(4);
     id = 4;  
 })
-
 saveBtnEl5.on('click', function () {
     saveInput(5);
     id = 5;  
 })
-
 saveBtnEl6.on('click', function () {
     saveInput(6);
     id = 6;  
 })
-
 saveBtnEl7.on('click', function () {
     saveInput(7);
     id = 7;  
 })
-
 saveBtnEl8.on('click', function () {
     saveInput(8);
     id = 8;  
 })
 
+//Fx that compares current time to timeblock and applies appropriate class styling
 function checkTime () {
     const timeBlocks = document.querySelectorAll(".form-control");
     for (var i = 0; i < timeBlocks.length; i++) {
@@ -103,6 +99,6 @@ function checkTime () {
 
 }
 
+//Calls init and checkTime fx's
 init ();
-
 checkTime();
